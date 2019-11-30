c2bc344eab79f0c147419d557a682405c96bd608
kouta1612 <kouta1612world69@gmail.com>
2019-11-30 22:04:02 +0900

    add trailing slash when redirect url

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
 
 class FormController extends Controller
 {
@@ -21,7 +22,13 @@ class FormController extends Controller
     public function complete(Request $request)
     {
         if ($request->action == 'back') {
-            return redirect('/create/')
+            // pattern ①
+            return redirect()->to(config('app.url') .  "create/") // 絶対URLあてにredirectすると、trailing slashが削除されないで済む
+                ->withInput($request->only('title')); // 入力データをフラッシュデータとして保存
+
+            // pattern ②
+            return redirect()
+                ->to('/create\/', 301) // trailing slashを作成するために"\"を加える
                 ->withInput($request->only('title')); // 入力データをフラッシュデータとして保存
         }
         Post::create(['title' => $request->title]);
